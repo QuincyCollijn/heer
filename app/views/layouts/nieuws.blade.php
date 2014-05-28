7e60609ab6cf290d3dcd7cd12d3895644fe271f9
Quincy Collijn <quincy.collijn@azion.nl>
2014-05-28 15:38:37 +0200

    Updating nieuws.blade.php & style.css

--- app/views/layouts/nieuws.blade.php
+++ app/views/layouts/nieuws.blade.php
@@ -2,24 +2,38 @@
 
 @section('content')
 
-
-<div class="image-container">
-    <img class="header-image" width="100%" height="200px;" src="/assets/events/svme-heer.png">
-</div>
-
-<div class="container"
-    <div class="row">
-        <div class="col-md-3">
-        <div class="sidebar-title-container">
-            <h1 class="sidebar-title">TEAM</h1>
-        </div>
-            <ul class="nav nav-pills nav-stacked ul-custom">
-              <li class="active-custom li-custom"><a href="#">Home</a></li>
-              <li class="li-custom"><a href="#">Profile</a></li>
-              <li class="li-custom"><a href="#">Messages</a></li>
-            </ul>
-       </div>
+<div class="container-fluid">
+<div class="row">
+    <img class="header-image" width="100%" height="200px"  src="/assets/events/training.jpg" />
+    <div class="container">
+        <h1 class="page-title">TEAM</h1>
     </div>
 </div>
+</div>
+
+            <nav class="navbar navbar-subpage navbar-default" role="navigation">
+              <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+
+
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                  <li class="active"><a href="#">NIEUWS</a></li>
+                    <li><a href="#">SELECTIE</a></li>
+                    <li><a href="#">STAFF</a></li>
+                    <li><a href="#">WEDSTRIJDEN & UITSLAGEN</a></li>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
 
 @stop
